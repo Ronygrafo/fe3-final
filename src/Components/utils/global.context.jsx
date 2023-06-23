3e23e04637755e1424858599e5ae2e06b8c66118
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2023-06-23 17:51:45 -0300

    add global context for api data

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -1,15 +1,26 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from 'react'
 
-export const initialState = {theme: "", data: []}
+export const initialState = { theme: '', data: [] }
 
-export const ContextGlobal = createContext(undefined);
+export const ContextGlobal = createContext(undefined)
 
 export const ContextProvider = ({ children }) => {
   //Aqui deberan implementar la logica propia del Context, utilizando el hook useMemo
+  const [dentists, setDentists] = useState([])
+
+  useEffect(() => {
+    const fetchDentists = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users')
+      const data = await response.json()
+      setDentists(data)
+    }
+
+    fetchDentists()
+  }, [])
 
   return (
-    <ContextGlobal.Provider value={{}}>
+    <ContextGlobal.Provider value={{ dentists }}>
       {children}
     </ContextGlobal.Provider>
-  );
-};
+  )
+}
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import { useContext } from 'react'
+import { ContextGlobal } from '../Components/utils/global.context'
 import Card from '../Components/Card'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
+  const { dentists } = useContext(ContextGlobal)
+
+  console.log(dentists)
   return (
-    <main className="" >
+    <main className=''>
       <h1>Home</h1>
       <div className='card-grid'>
         {/* Aqui deberias renderizar las cards */}
