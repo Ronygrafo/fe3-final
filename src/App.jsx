3720e1595f59968087b69d659d91b34d3a3f0a11
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2023-06-23 10:22:05 -0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Footer from './Components/Footer'
 import { Header } from './Components/Header/Header'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/contact' element={<h1>Contact</h1>} />
+        <Route path='/favs' element={<h1>Favs</h1>} />
+      </Routes>
       <Footer />
     </div>
   )
--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -8,9 +8,15 @@ const Navbar = () => {
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
       <ul>
-        <li>Home</li>
-        <li>Contact</li>
-        <li>Favs</li>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/contact'>Contact</Link>
+        </li>
+        <li>
+          <Link to='/favs'>Favs</Link>
+        </li>
         <button>Change theme</button>
       </ul>
     </nav>
