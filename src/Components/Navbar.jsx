ebe16493cc0278e50accb24f0472bc13dc32e391
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2023-06-23 08:26:47 -0300

    add navbar items

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,12 +3,17 @@ import React from 'react'
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
-
   return (
     <nav>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
+      <h1>DH Odonto</h1>
+      <ul>
+        <li>Home</li>
+        <li>Contact</li>
+        <li>Favs</li>
+        <button>Change theme</button>
+      </ul>
     </nav>
   )
 }
