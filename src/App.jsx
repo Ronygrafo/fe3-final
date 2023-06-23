02f0aa34e317e84169f02b0ea57d4ee3518234e3
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2023-06-23 10:33:10 -0300

    add component to routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import Footer from './Components/Footer'
 import { Header } from './Components/Header/Header'
 import { Routes, Route } from 'react-router-dom'
+import Home from './Routes/Home'
+import Contact from './Routes/Contact'
+import Favs from './Routes/Favs'
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <Routes>
-        <Route path='/' element={<h1>Home</h1>} />
-        <Route path='/contact' element={<h1>Contact</h1>} />
-        <Route path='/favs' element={<h1>Favs</h1>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/favs' element={<Favs />} />
       </Routes>
       <Footer />
     </div>
