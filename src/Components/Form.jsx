6a4a2d0a4242e4434b0d17031c152e217622fbec
Rony Romero Duque @ronygrafo <ronygrafo@icloud.com>
2023-06-25 08:29:30 -0500

    name & email validations

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -6,71 +6,97 @@ const Form = () => {
   //Aqui deberan implementar el form completo con sus validaciones
 
   const [leadData, setLeadData] = useState({
-    name: "",
-    email: "",
+    name: {value: "", isOK: null},
+    email: {value: "", isOK: null},
   });
 
-  const [show, setShow] = useState(false)
+/*   function checkInputsData() {
+
+    if (leadData.name.isOK === null) {
+
+    }
+
+  } */
 
   const errMessage = {
-    errName: "Tu nombre no es válido, por favor verificalo",
+    errName: "Tu Nombre Completo es muy corto!, por favor verificalo",
     errEmail: "Porfa, danos tu Email correcto 🙂",
   }
 
   const handleNameData = (e) =>{
-    // Validar NOMBRE con longitud mayor a 5 y sin Espacio en blanco al iniciar
+    // Validar NOMBRE con longitud mayor a 5 y sin espacio en blanco al iniciar
 
-    // console.log(e.target.value.length > 5 && (e.target.value.charAt(0) !== " "));
     if (e.target.value.length > 5 && (e.target.value.charAt(0) !== " ")){
       setLeadData({
         ...leadData,
-        name: e.target.value})
+        name: {value: e.target.value, isOK: true}})
     } else {
-      console.log(errMessage.errName);
+      setLeadData({
+        ...leadData,
+        name: {value: "", isOK: false}})
     }
-    //console.log(leadData);
   }
 
   const handleEmailData = (e) =>{
     // Validar EMAIL válido con REGEX
     const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
-    //console.log(regex.test(e.target.value));
-
     if (regex.test(e.target.value)){
       setLeadData({
         ...leadData,
-        email: e.target.value})
-    }else {
-      console.log(errMessage.errEmail);
+        email: {value: e.target.value, isOK: true}})
+    } else {
+      setLeadData({
+        ...leadData,
+        email: {value: "", isOK: false}})
     }
-    //console.log(leadData);
   }
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(`LEAD RECIBIDO: \n Nombre: ${leadData.name} \n Email: ${leadData.email} \n ========================`);
+
+    //console.log(`Hay errores?`);
+
+ 
+
+    console.log(leadData.name.isOK);
+    console.log(leadData.email.isOK);
+    console.log("-");
+
+    //checkInputs() ? console.log("Hay errores") : console.log(`LEAD RECIBIDO: \n Nombre: ${leadData.name.value} \n Email: ${leadData.email.value} \n ========================`)
+
+    //(checkInputs())? console.log("Hay errores") : console.log(`LEAD RECIBIDO: \n Nombre: ${leadData.name.value} \n Email: ${leadData.email.value} \n ========================`);
+
+/*     if (leadData.name.error === true || leadData.name.error === true) {
+      console.log("Hay errores")
+    } else{
+      //console.log(`LEAD RECIBIDO: \n Nombre: ${leadData.name} \n Email: ${leadData.email} \n ========================`);
+      console.log(leadData)
+     } */
   }
 
   return (
     <>
-    <div>
+    <div className="contactForm">
     <form onSubmit={handleSubmit}>
 
       <input type="text" name="name" placeholder="Nombre completo"
       onChange={handleNameData}/>
 
+      {leadData.name.isOK ? <></> :  <p>{errMessage.errName}</p>}
+      
+
       <input type="text" name="email" placeholder="Tu e-mail"
       onChange={handleEmailData}/>
+
+      {leadData.email.isOK ? <></> :  <p>{errMessage.errEmail}</p>}
 
       <button type="submit"> Enviar </button>
 
     </form>
     </div>
-    <p>
-      Mensaje output Aqui
-    </p>
+
     </>
   );
 };
