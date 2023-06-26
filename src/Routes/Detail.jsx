baf01b67a5b35bb70fa0a9a20669c58ddb9f8cda
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2023-06-26 08:59:07 -0300

    update back btn style

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,6 +1,7 @@
 import { useParams, useNavigate } from 'react-router-dom'
 import { useContext } from 'react'
 import { ContextGlobal } from '../Components/utils/global.context'
+import { BsArrowLeft } from 'react-icons/bs'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -14,25 +15,28 @@ const Detail = () => {
   return (
     <>
       {/* <h1>Dentist id {currentDentist?.id}</h1> */}
-      
-      <button className='back-btn' onClick={() => navigate(-1)}>Go back</button>
+
+      <button className='back-btn' onClick={() => navigate(-1)}>
+        <BsArrowLeft />
+      </button>
       <h1 className='detail-heading'>{currentDentist?.name}</h1>
 
       {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
       {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
 
       <div className='card-grid'>
-      <div className='card' style={{width: '260px'}} >
+        <div className='card' style={{ width: '260px' }}>
           <img src='../images/doctor.jpg' alt='doctor' />
           <p>{currentDentist?.name}</p>
-          <strong><p>ID: {currentDentist?.id}</p></strong> 
+          <strong>
+            <p>ID: {currentDentist?.id}</p>
+          </strong>
           <p>{currentDentist?.email}</p>
           <p>{currentDentist?.phone}</p>
           <p>{currentDentist?.website}</p>
         </div>
       </div>
     </>
-
   )
 }
 
