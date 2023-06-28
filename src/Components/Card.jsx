506aea30881822e264c7008d2420e8b51160fd12
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2023-06-28 18:33:31 -0300

    fix img on slow conections

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,11 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { ContextGlobal } from '../Components/utils/global.context'
 import { Link } from 'react-router-dom'
 import { BsHeart, BsHeartFill } from 'react-icons/bs'
 
 const Card = ({ name, username, id }) => {
   const { favState, favDispatch } = useContext(ContextGlobal)
+  const [imageLoaded, setImageLoaded] = useState(false)
 
   const isFav = favState.some(fav => fav.id === id)
 
@@ -18,10 +19,20 @@ const Card = ({ name, username, id }) => {
     }
   }
 
+  const handleImageLoad = () => {
+    setImageLoaded(true)
+  }
+
   return (
     <div className='card-wraper'>
       <Link className='card' to={`/detail/${id}`}>
-        <img src='./images/doctor.jpg' alt='doctor' />
+        {!imageLoaded && <div className='skeleton_img'></div>}
+        <img
+          src='./images/doctor.jpg'
+          alt='doctor'
+          onLoad={handleImageLoad}
+          style={{ display: imageLoaded ? 'block' : 'none' }}
+        />
         {/* En cada card deberan mostrar en name - username y el id */}
         <p>{name}</p>
         <p>User: {username}</p>
