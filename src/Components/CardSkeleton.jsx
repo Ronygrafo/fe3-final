0aca944b5786b4160a32c54ee7d1236aefb10fde
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2023-06-28 15:41:54 -0300

    add skeleton

---
+++ src/Components/CardSkeleton.jsx
@@ -0,0 +1,10 @@
+export default function CardSkeleton() {
+  return (
+    <div className='card skeleton_container'>
+      <div className='skeleton_img'></div>
+      <p className='skeleton_info'></p>
+      <p className='skeleton_info'></p>
+      <p className='skeleton_info'></p>
+    </div>
+  )
+}
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { ContextGlobal } from '../Components/utils/global.context'
 import Card from '../Components/Card'
+import CardSkeleton from '../Components/CardSkeleton'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -14,10 +15,17 @@ const Home = () => {
       <h1>Los Mejores Dentistas</h1>
       <p>a un solo click</p>
       <div className='card-grid'>
-        {/* Aqui deberias renderizar las cards */}
-        {dentistsState.dentistsList.map(dentist => (
-          <Card key={dentist.id} {...dentist} />
-        ))}
+        {dentistsState.dentistsList.length === 0 ? (
+          <>
+            {[...Array(10)].map((_, index) => (
+              <CardSkeleton key={index} />
+            ))}
+          </>
+        ) : (
+          dentistsState.dentistsList.map(dentist => (
+            <Card key={dentist.id} {...dentist} />
+          ))
+        )}
       </div>
     </>
   )
